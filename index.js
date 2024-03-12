f28aa560fa5c3a24307352b5f01b91768fc6543e
Hoa Ngo <hngo34048@gmail>
2024-03-12 23:32:41 +0700

    update link them bootstrap

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
 const route = require("./routes/client/index.route");
+app.use(express.static('public'));
 app.set('views','./views');
 app.set('view engine','pug');
 route(app);
