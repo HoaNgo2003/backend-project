84cb5cd0186ac47c4ebbbd60a003243f97802d7f
Hoa Ngo <hngo34048@gmail>
2024-03-13 13:52:24 +0700

    tao trang admin

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const database = require("./config/database");
 database.connect();
 const port = process.env.PORT;
 const route = require("./routes/client/index.route");
+const routeAdmin = require("./routes/admin/index.route");
 app.use(express.static('public'));
 app.set('views','./views');
 app.set('view engine','pug');
 route(app);
+routeAdmin(app);
 app.listen(port, ()=>{
     console.log(port);
 })
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -3,8 +3,8 @@ const homeRoutes = require("./home.route");
 module.exports = (app)=>{
     app.get('/',(req,res)=>{
         res.render("client/pages/home/index");
-     })
-     app.use("/",homeRoutes);
+    })
+    app.use("/",homeRoutes);
     app.use("/products",productRoutes);
     
 }
