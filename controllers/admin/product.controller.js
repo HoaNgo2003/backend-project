e81f50043b2a448e22e2c0b691c921d99c0233dc
Hoa Ngo <hngo34048@gmail>
2024-03-14 23:10:22 +0700

    phan trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const products = require("../../models/product.model");
 const filterStatus = require("../../helpers/filterStatus");
 const searchStatus = require("../../helpers/searchStatus");
+const Product = require("../../models/product.model");
 module.exports.index= async(req, res) => {
   const filterStatus1 = filterStatus(req.query);
   let find = {
@@ -11,16 +12,30 @@ module.exports.index= async(req, res) => {
     find.status = req.query.status;
   }
  ///
+//  pagination
+  let objectPanigation = {
+    limitItems: 4,
+    currentPage: 1
+  }
+// done pagination
+  if(req.query.page){
+    objectPanigation.currentPage = parseInt(req.query.page);
+  }
+  objectPanigation.skip = (objectPanigation.currentPage-1)*objectPanigation.limitItems;
   const keyword = searchStatus(req.query);
   if(keyword.regex){
     find.title = keyword.regex;
   }
-  const product = await products.find(find);
+  const countProduct = await Product.countDocuments(find);
+  const totalPage = Math.ceil(countProduct/objectPanigation.limitItems);
+  objectPanigation.totalPage = totalPage;
+  const product = await products.find(find).limit(objectPanigation.limitItems).skip(objectPanigation.skip);
   res.render("admin/pages/products/index.pug", {
       pageTitle: "Trang admin",
       product: product,
       filterStatus: filterStatus1,
-      keyword: keyword.keyword
+      keyword: keyword.keyword,
+      pagination: objectPanigation
     });
     // res.send("Trang san pham");
   }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -34,3 +34,17 @@ if(formSearch){
         window.location.href = url.href;
     })
 } 
+// pagination
+const pagination = document.querySelectorAll("[button-pagination]");
+console.log(pagination);
+if(pagination){
+    let url = new URL(window.location.href);
+    pagination.forEach(button=>{
+        button.addEventListener("click",()=>{
+            const page = button.getAttribute("button-pagination");
+            url.searchParams.set("page",page);
+            window.location.href = url.href;
+        })
+    })
+}
+//done
